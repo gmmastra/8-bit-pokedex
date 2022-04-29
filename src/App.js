f8a6f30f468684de24d35f96c1bfedd5883d2f3f
gmmastra <gmmastra@ncsu.edu>
2022-04-29 04:39:07 -0400

    Fixed routing paths

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { PkmnList } from "./components/PkmnList";
 
 export default function App() {
-
   return (
     <Router>
       <div className="App">
         {/* navbar */}
         <div className="content">
           <Switch>
-            <Route exact path="/" component={PkmnList} />
-            <Route exact path="/index" component={PkmnList} />
+            <Route exact path="/8-bit-pokedex/" component={PkmnList} />
+            <Route exact path="/8-bit-pokedex/index" component={PkmnList} />
             <Route component={NotFound} />
           </Switch>
         </div>
