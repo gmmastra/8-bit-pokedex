2bfe84a48e121f2d17d905d9a08bf5e71102fc2e
gmmastra <gmmastra@ncsu.edu>
2022-04-29 02:59:29 -0400

    Refactored App into Router, added sprite anims

--- src/App.js
+++ src/App.js
@@ -1,78 +1,23 @@
 import React, { useEffect } from "react";
-import axios from "axios";
-import Box from '@mui/material/Box';
-import { PkmnInfoCard } from "./components/PkmnInfoCard";
-import Loading from "./assets/loading.gif";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+import { PkmnList } from "./components/PkmnList";
 
-const App = () => {
-
-  const [pkmnMap, setPkmnMap] = React.useState([]);
-  const [position, setPosition] = React.useState(1);
-  const [initialize, setInitialize] = React.useState(true);
-  const [loading, setLoading] = React.useState(false);
-
-  useEffect(() => {
-    fetchPokemon();
-    // eslint-disable-next-line
-  }, [])
-
-  // Axios GET response from pokeapi
-  const fetchPokemon = async () => {
-    setLoading(true);
-    var chunkSize;
-    initialize ? chunkSize = 75 : chunkSize = 25;
-    const promiseChunk = [];
-
-    // array of promises (requests)
-    for (let i = position; i < position + chunkSize; i++) {
-      promiseChunk.push(
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
-          .then(({ data }) => (data))
-      );
-    }
-    // wait to execute in parallel
-    const temp = await axios.all(promiseChunk);
-    if (initialize) {
-      setPkmnMap(temp);
-      setInitialize(false);
-    } else {
-      setPkmnMap((prevState) => [
-        ...prevState,
-        ...temp,
-      ]);
-    }
-    setPosition(position + chunkSize);
-    setLoading(false);
-  }
-
-  // Fetch when near bottom of page
-  const handleScroll = (e) => {
-    const bottom = e.target.scrollHeight - e.target.scrollTop < e.target.clientHeight + 10;
-    if (bottom) {
-      fetchPokemon();
-    }
-  }
+export default function App() {
 
   return (
-    <div style={{ textAlign: 'center' }}>
-
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '80px' }}>
-        {/* DISPLAY INFOCARDS */}
-        <Box className="info-card-wrapper" sx={{ gridTemplateColumns: { sm: 'repeat(auto-fit, minmax(7.5rem, 1fr))' } }} onScroll={handleScroll} >
-          {pkmnMap.map((e) => {
-            return <PkmnInfoCard key={e.id}
-              id={e.id}
-              name={e.name}
-              types={e.types}
-              sprite={e.sprites.versions["generation-viii"].icons.front_default} />;
-          })}
-        </Box>
+    <Router>
+      <div className="App">
+        {/* navbar */}
+        <div className="content">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/index" component={PkmnList} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
       </div>
-
-      {loading && <img style={{ height: '50px', opacity: '.5' }} src={Loading} alt="loading..." />}
-
-    </div>
+    </Router>
   );
 }
-
-export default App;
